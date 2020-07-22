2239a58f46a374d780212bdf981dc953d79b617e
Md. Omar Faruk <iamomar022@gmail.com>
2020-07-22 10:31:01 +0600

    Question and Answer Model

--- courses/admin.py
+++ courses/admin.py
@@ -1,16 +1,9 @@
 from django.contrib import admin
 
-from .models import Course, Text, Quiz
+from . import models
 
-class TextInline(admin.StackedInline):
-    model = Text
-
-
-class CourseAdmin(admin.ModelAdmin):
-    inlines = [TextInline,]
-
-
-
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Text)
-admin.site.register(Quiz)
+admin.site.register(models.Course)
+admin.site.register(models.Text)
+admin.site.register(models.Quiz)
+admin.site.register(models.Question)
+admin.site.register(models.Answer)
--- courses/models.py
+++ courses/models.py
@@ -40,3 +40,31 @@ class Quiz(Step):
 
     def get_absolute_url(self):
         return reverse('quiz_detail', kwargs={'course_pk': self.course_id, 'step_pk': self.id})
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    order = models.IntegerField(default=0)
+    prompt = models.TextField()
+
+    class Meta:
+        ordering = ['order',]
+
+    def get_absolute_url(self):
+        return self.quiz.get_absolute_url()
+
+    def __str__(self):
+        return self.prompt
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    order = models.IntegerField(default=0)
+    text = models.CharField(max_length=255)
+    correct = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['order',]
+
+    def __str__(self):
+        return self.text
