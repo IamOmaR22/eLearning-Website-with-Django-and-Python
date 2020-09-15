de91e174df392152375bd61f4cd2d713ba0ef07f
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-15 12:01:57 +0600

    Forms and Templates

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,4 +4,17 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
-    pass
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    email = forms.EmailField(max_length=254, help_text='Enter a valid email address')
+
+    class Meta:
+        model = User
+        fields = [
+            'username', 
+            'first_name', 
+            'last_name', 
+            'email', 
+            'password1', 
+            'password2', 
+            ]
