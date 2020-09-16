a1678b9e24a7bf4d9ae98af857638c33432591f0
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-16 09:22:38 +0600

    Logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
 ]
