99cfe343e5dcfe3659adb5cb64bb65fed9835137
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-16 09:13:07 +0600

    Routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignUpView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
