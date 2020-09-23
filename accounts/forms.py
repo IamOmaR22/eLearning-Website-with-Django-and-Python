2137f62e5d4a645163741ad4f8c0fd2517095153
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-23 08:43:00 +0600

    Update forms

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class SignUpForm(UserCreationForm):
@@ -18,3 +19,17 @@ class SignUpForm(UserCreationForm):
             'password1', 
             'password2', 
             ]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()  # This For Additional Field
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
