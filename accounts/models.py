61c8ed44d8465ba32062383856b8358440436201
Md. Omar Faruk <iamomar022@gmail.com>
2020-09-23 08:58:06 +0600

    Update Profile Done

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics', null=True, blank=True)
 
 
     def __str__(self):
